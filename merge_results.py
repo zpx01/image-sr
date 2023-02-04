bf03ca0ea424a020fb2d727fed0fbbd9c0dee311
Zeeshan Patel zeeshanp@berkeley.edu <zeeshanp@berkeley.edu>
2023-02-04 07:32:31 +0000

    updated merge class, uses y-channel

--- merge_results.py
+++ merge_results.py
@@ -1,10 +1,11 @@
 import os, glob, argparse
 import numpy as np
 from data_loader_utils import *
-import matplotlib.image as mpimg
+import cv2
 from collections import defaultdict
 import torch
 import skimage
+from utils import utils_calculate_psnr_ssim as util
 
 res_strs = []
 class MergeResults(object):
@@ -15,48 +16,48 @@ class MergeResults(object):
     a new, merged inference. It provides the PSNR and SSIM for each of the three
     image categories.
     """
-    def __init__(self, model_path_1, model_path_2, gt_path, merged_img_path):
+    def __init__(self, model_path_1, model_path_2, gt_path, lr_path, merged_img_path, scale):
         self.model_path_1 = model_path_1 # original pretrained model inference
         self.model_path_2 = model_path_2 # ttt model inference 
         self.gt_path = gt_path
+        self.lr_path = lr_path
         self.merged_img_path = merged_img_path
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.psnr_values = defaultdict(list)
         self.ssim_values = defaultdict(list)
+        self.mse_values = defaultdict(list)
+        self.scale = scale
+        self.window_size = 8
     
     def merge_results(self):
         # load images
         global res_strs
-        img1 = mpimg.imread(self.model_path_1)
-        img2 = mpimg.imread(self.model_path_2)
-        gt = mpimg.imread(self.gt_path)
-
+        img1 = cv2.imread(self.model_path_1, cv2.IMREAD_COLOR)
+        img2 = cv2.imread(self.model_path_2, cv2.IMREAD_COLOR)
+        gt = cv2.imread(self.gt_path, cv2.IMREAD_COLOR)
+        lr = cv2.imread(self.lr_path, cv2.IMREAD_COLOR)
+        lr = np.transpose(lr if lr.shape[2] == 1 else lr[:, :, [2, 1, 0]], (2, 0, 1))  # HCW-BGR to CHW-RGB
+        lr = torch.from_numpy(lr).float().unsqueeze(0)  # CHW-RGB to NCHW-RGB
+        _, _, h_old, w_old = lr.size()
+        gt = gt[:h_old * args.scale, :w_old * args.scale, ...]  # crop gt
+        gt = np.squeeze(gt)
         (imgname, imgext) = os.path.splitext(os.path.basename(self.gt_path))
         s = f"Testing {imgname} ({self.gt_path})...\n"
         print(s)
         res_strs.append(s)
-
         # compute individual PSNR/SSIM values
-        original_psnr, ttt_psnr = self.psnr(gt, img1), self.psnr(gt, img2)
-        original_ssim, ttt_ssim = self.ssim(gt, img1), self.ssim(gt, img2)
+        original_psnr, ttt_psnr = self.psnr(gt, img1, crop_border=self.scale), self.psnr(gt, img2, crop_border=self.scale)
+        original_ssim, ttt_ssim = self.ssim(gt, img1, crop_border=self.scale), self.ssim(gt, img2, crop_border=self.scale)
 
         # create merged image
-        merged_img = np.zeros(shape=gt.shape, dtype=np.float32)
-        for i in range(len(img1)):
-            for j in range(len(img1[i])):
-                v1, v2, gt_v = img1[i][j], img2[i][j], gt[i][j]
-                norm1, norm2 = self.l2_norm_pixel(gt_v, v1), self.l2_norm_pixel(gt_v, v2)
-                if norm1 <= norm2:
-                    merged_img[i][j] = img1[i][j]
-                elif norm2 < norm1:
-                    merged_img[i][j] = img2[i][j]
+        merged_img, mse_orig, mse_ttt, mse_merged = self.merge_images(img1, img2, gt)
 
         # save merged image
-        mpimg.imsave(self.merged_img_path, merged_img)
+        cv2.imwrite(self.merged_img_path, merged_img)
         
         # compute merged PSNR/SSIM values
-        merged_psnr = self.psnr(gt, merged_img)
-        merged_ssim = self.ssim(gt, merged_img)
+        merged_psnr = self.psnr(gt, merged_img, crop_border=self.scale)
+        merged_ssim = self.ssim(gt, merged_img, crop_border=self.scale)
 
         # save metrics
         self.psnr_values["original"].append(original_psnr)
@@ -65,20 +66,32 @@ class MergeResults(object):
         self.ssim_values["original"].append(original_ssim)
         self.ssim_values["ttt"].append(ttt_ssim)
         self.ssim_values["merged"].append(merged_ssim)
+        self.mse_values["original"].append(mse_orig)
+        self.mse_values["ttt"].append(mse_ttt)
+        self.mse_values["merged"].append(mse_merged)
 
-    def psnr(self, img_gt, output):
-        psnr = skimage.metrics.peak_signal_noise_ratio(img_gt, output)
+    def psnr(self, img_gt, output, crop_border):
+        psnr = util.calculate_psnr(output, img_gt, crop_border=crop_border, test_y_channel=True)
         psnr = float('{:.2f}'.format(psnr))
         return psnr
     
-    def ssim(self, img_gt, output):
-        ssim = skimage.metrics.structural_similarity(img_gt, output, channel_axis=1, multichannel=True)
+    def ssim(self, img_gt, output, crop_border):
+        ssim = util.calculate_ssim(output, img_gt, crop_border=crop_border, test_y_channel=True)
         ssim = float('{:.2f}'.format(ssim))
         return ssim
 
-    def l2_norm_pixel(self, v1, v2):
-        return np.sum((v1 - v2)**2)
-
+    def merge_images(self, inf_img1, inf_img2, gt_img):
+        inf_img1_y, inf_img2_y, gt_img_y = util.to_y_channel(inf_img1), util.to_y_channel(inf_img2), util.to_y_channel(gt_img)
+        loss1 = np.sum((inf_img1_y - gt_img_y) ** 2, axis=-1)
+        loss2 = np.sum((inf_img2_y - gt_img_y) ** 2, axis=-1)
+        loss1 = loss1[..., np.newaxis]
+        loss2 = loss2[..., np.newaxis]
+        merged_img = np.where(loss1 < loss2, inf_img1, inf_img2)
+        mse_orig = np.mean((inf_img1_y - gt_img_y) ** 2)
+        mse_ttt = np.mean((inf_img2_y - gt_img_y) ** 2)
+        merged_y = util.to_y_channel(merged_img)
+        mse_merged = np.mean((merged_y - gt_img_y) ** 2)
+        return merged_img, mse_orig, mse_ttt, mse_merged
 
 def get_args_parser():
     parser = argparse.ArgumentParser('MergeResults - Classical SR', add_help=False)
@@ -86,6 +99,8 @@ def get_args_parser():
     parser.add_argument('--ttt_dir', type=str, help='path to TTT model inference images')
     parser.add_argument('--gt_dir', type=str, help='path to ground truth images')
     parser.add_argument('--merged_dir', type=str, help='path to directory for merged image results')
+    parser.add_argument('--lr_dir', type=str, help='path to low res images')
+    parser.add_argument('--scale', type=int, default=4)
     parser.add_argument('--results_log', type=str, help='path to text file to save metrics')
     return parser
 
@@ -106,42 +121,51 @@ def main(args):
     for idx, path in enumerate(sorted(glob.glob(os.path.join(model_2_results_dir, '*')))):
         model_2_img_paths.append(path)
 
+    lr_dir = args.lr_dir
+    lr_paths = []
+    for idx, path in enumerate(sorted(glob.glob(os.path.join(lr_dir, '*')))):
+        lr_paths.append(path)
+
     test_psnr_vals = defaultdict(list)
     test_ssim_vals = defaultdict(list)
+    test_mse_vals = defaultdict(list)
     for i in range(len(test_img_paths)):
         (imgname, imgext) = os.path.splitext(os.path.basename(test_img_paths[i]))
         merged_path = f'{args.merged_dir}/{imgname}_merged{imgext}'
-        mr = MergeResults(model_1_img_paths[i], model_2_img_paths[i], test_img_paths[i], merged_path)
+        mr = MergeResults(model_1_img_paths[i], model_2_img_paths[i], test_img_paths[i], lr_paths[i], merged_path, args.scale)
         mr.merge_results()
         for key, val in mr.psnr_values.items():
             if key == 'original':
                 test_psnr_vals['original'].append(val[0])
                 test_ssim_vals['original'].append(mr.ssim_values[key][0])
-                s = f"Original PSNR: {val[0]}; Original SSIM: {mr.ssim_values[key][0]}\n"
+                test_mse_vals['original'].append(mr.mse_values[key][0])
+                s = f"Original PSNR: {val[0]}; Original SSIM: {mr.ssim_values[key][0]}; Original MSE: {mr.mse_values[key][0]}\n"
                 res_strs.append(s)
                 print(s)
             elif key == 'ttt':
                 test_psnr_vals['ttt'].append(val[0])
                 test_ssim_vals['ttt'].append(mr.ssim_values[key][0])
-                s = f"TTT PSNR: {val[0]}; TTT SSIM: {mr.ssim_values[key][0]}\n"
+                test_mse_vals['ttt'].append(mr.mse_values[key][0])
+                s = f"TTT PSNR: {val[0]}; TTT SSIM: {mr.ssim_values[key][0]}; TTT MSE: {mr.mse_values[key][0]}\n"
                 res_strs.append(s)
                 print(s)
             else:
                 test_psnr_vals['merged'].append(val[0])
                 test_ssim_vals['merged'].append(mr.ssim_values[key][0])
-                s = f"Merged PSNR: {val[0]}; Merged SSIM: {mr.ssim_values[key][0]}\n\n"
+                test_mse_vals['merged'].append(mr.mse_values[key][0])
+                s = f"Merged PSNR: {val[0]}; Merged SSIM: {mr.ssim_values[key][0]}; Merged MSE: {mr.mse_values[key][0]}\n\n"
                 res_strs.append(s)
                 print(s)
     s = f'\n\nAverage Statistics for Test Set:\n'
     res_strs.append(s)
     print(s)
-    s = f"Original Average PSNR: {np.mean(test_psnr_vals['original'])}; Original Average SSIM: {np.mean(test_ssim_vals['original'])}\n"
+    s = f"Original Average PSNR: {np.mean(test_psnr_vals['original'])}; Original Average SSIM: {np.mean(test_ssim_vals['original'])}; Original Average MSE: {np.mean(test_mse_vals['original'])}\n"
     res_strs.append(s)
     print(s)
-    s = f"TTT Average PSNR: {np.mean(test_psnr_vals['ttt'])}; TTT Average SSIM: {np.mean(test_ssim_vals['ttt'])}\n"
+    s = f"TTT Average PSNR: {np.mean(test_psnr_vals['ttt'])}; TTT Average SSIM: {np.mean(test_ssim_vals['ttt'])}; TTT Average MSE: {np.mean(test_mse_vals['ttt'])}\n"
     res_strs.append(s)
     print(s)
-    s = f"Merged Average PSNR: {np.mean(test_psnr_vals['merged'])}; Merged Average SSIM: {np.mean(test_ssim_vals['merged'])}\n"
+    s = f"Merged Average PSNR: {np.mean(test_psnr_vals['merged'])}; Merged Average SSIM: {np.mean(test_ssim_vals['merged'])}; Merged Average MSE: {np.mean(test_mse_vals['merged'])}\n"
     res_strs.append(s)
     print(s)
     res_strs.append('\n')
